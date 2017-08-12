fc060dea8e06d12c9b46e119836520a783291aa4
MaxOvcharov <ovcharovmax@yandex.ru>
2017-08-12 22:30:31 +0300

    Create Redis Client and client factory for it

---
+++ redis_client.py
@@ -0,0 +1,37 @@
+import aioredis
+
+
+class RedisClient:
+    """ Redis connection pool client """
+
+    def __init__(self, loop, conf):
+        self.loop = loop
+        self.conf = conf
+        self.rd = None
+
+    @classmethod
+    async def connect(cls, **options):
+        self = cls(**options)
+        await self.init_connection()
+        return self
+
+    async def init_connection(self):
+        self.rd = await aioredis.create_pool(
+            (self.conf['host'], self.conf['port']),
+            db=self.conf['db'],
+            password=self.conf['password'],
+            maxsize=self.conf['maxsize'],
+            minsize=self.conf['minsize'],
+            loop=self.conf['loop']
+        )
+
+    async def close_connection(self):
+        self.rd.close()
+        await self.rd.wait_closed()
+
+
+async def rd_client_factory(loop, conf, client=RedisClient):
+    """ Abstract Redis client factory """
+    rd_client = client(loop, conf)
+    rd = await rd_client.connect(loop=loop, conf=conf)
+    return rd
