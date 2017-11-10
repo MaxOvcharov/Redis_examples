d24966aa907e1a27eaf6ac7d9b7bedf399c5c68e
MaxOvcharov <ovcharovmax@yandex.ru>
2017-11-10 23:48:36 +0300

    Added Scripting Command examples

---
+++ exp9_scripting_cmd/async_scripting_cmd.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+    Simple example of Redis Scripting commands using async lib - aioredis
+    For commands details see: http://redis.io/commands#scripting
+"""
+import asyncio
+import os
+
+from itertools import chain
+from random import choice, randint
+
+from redis_client import rd_client_factory
+from settings import BASE_DIR, logger
+from utils import load_config
+
+
+class RedisScriptingCommands:
+    def __init__(self, rd1, rd2, conf=None):
+        self.rd1 = rd1
+        self.rd2 = rd2
+        self.rd_conf = conf
+
+    async def run_scripting_cmd(self):
+        pass
+
+
+def main():
+    # load config from yaml file
+    conf = load_config(os.path.join(BASE_DIR, "config_files/dev.yml"))
+    # create event loop
+    loop = asyncio.get_event_loop()
+    # rd1 = await RedisClient.connect(conf=conf['redis'], loop=loop)
+    # rd2 = await RedisClient.connect(conf=conf['redis'], loop=loop)
+    rd1_conn = loop.run_until_complete(rd_client_factory(loop=loop, conf=conf['redis1']))
+    rd2_conn = loop.run_until_complete(rd_client_factory(loop=loop, conf=conf['redis2']))
+    rgc = RedisScriptingCommands(rd1_conn.rd, rd2_conn.rd, conf=conf['redis2'])
+    try:
+        loop.run_until_complete(rgc.run_scripting_cmd())
+    except KeyboardInterrupt as e:
+        logger.error("Caught keyboard interrupt {0}\nCanceling tasks...".format(e))
+    finally:
+        loop.run_until_complete(rd1_conn.close_connection())
+        loop.run_until_complete(rd2_conn.close_connection())
+        loop.close()
+
+
+if __name__ == '__main__':
+    main()
