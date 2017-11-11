55ef871ffdd1019fe09ee58bc36f13e6ae7ea435
MaxOvcharov <ovcharovmax@yandex.ru>
2017-11-11 23:36:27 +0300

    Added SCRIPTING CMD - EVAL

--- exp9_scripting_cmd/async_scripting_cmd.py
+++ exp9_scripting_cmd/async_scripting_cmd.py
@@ -21,7 +21,35 @@ class RedisScriptingCommands:
         self.rd_conf = conf
 
     async def run_scripting_cmd(self):
-        pass
+        await self.rd_eval_cmd()
+
+    async def rd_eval_cmd(self):
+        """
+        EVAL and EVALSHA are used to evaluate scripts
+          using the Lua interpreter built into Redis
+          starting from version 2.6.0.
+          - The first argument of EVAL is a Lua 5.1 script.
+          The script does not need to define a Lua function
+          (and should not). It is just a Lua program that
+          will run in the context of the Redis server.
+          - The second argument of EVAL is the number of
+          arguments that follows the script (starting from
+          the third argument) that represent Redis key names.
+          The arguments can be accessed by Lua using the KEYS
+          global variable in the form of a one-based array
+          (so KEYS[1], KEYS[2], ...).
+          - All the additional arguments should not represent
+          key names and can be accessed by Lua using the ARGV
+          global variable, very similarly to what happens with
+          keys (so ARGV[1], ARGV[2], ...).
+
+        :return: None
+        """
+        script_cmd = "return {1,2,{3,'Hello World!'}}"
+        with await self.rd1 as conn:
+            res1 = await conn.eval(script_cmd, args=[0])
+        frm = "SORTED_SCRIPTING_CMD - 'EVAL': SCRIPT_RES_VALUE - {0}\n"
+        logger.debug(frm.format(res1))
 
 
 def main():
