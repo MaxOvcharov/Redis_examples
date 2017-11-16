c95c5a079b473753888e3652c52c49b095aef4d3
MaxOvcharov <ovcharovmax@yandex.ru>
2017-11-16 21:59:26 +0300

    Added PUBSUB CMD - PUBLISH - Fixed RedisSubWorker

--- exp10_pubsub_cmd/async_pubsub_cmd.py
+++ exp10_pubsub_cmd/async_pubsub_cmd.py
@@ -21,9 +21,9 @@ class RedisSubWorker:
     @classmethod
     async def connect(cls, *args, **kwargs):
         """
-        This method creates Redis client.
+        This method creates Redis Subscriber.
 
-        :return obj self: RedisClient instance
+        :return obj self: RedisSubWorker instance
         """
         self = cls(*args, **kwargs)
         await self._init_connect()
@@ -31,18 +31,18 @@ class RedisSubWorker:
 
     async def _init_connect(self):
         """
+        This method init Redis Subscriber which reads msg
+          from queue.
 
-        :return:
+        :return: None
         """
         with await self.rd as conn:
             ch = await conn.subscribe(self.channel)
-        print(ch)
-        while await ch[0].wait_message():
-            msg = await ch[0].get(encoding='utf-8')
-            frm = "PUBSUB_CMD - SUB_RESULT - {0}\n"
-            logger.debug(frm.format(msg))
-            msg = await ch[0].get(encoding='utf-8')
-            print(msg)
+            while await ch[0].wait_message():
+                msg = await ch[0].get(encoding='utf-8')
+                frm = "PUBSUB_CMD - SUB_RESULT - {0}\n"
+                logger.debug(frm.format(msg))
+
 
 class RedisPubSubCommands:
     def __init__(self, rd1, rd2, conf=None):
@@ -52,23 +52,8 @@ class RedisPubSubCommands:
 
     async def run_pubsub_cmd(self):
         await self.pubsub_publish_cmd()
-        await self.pubsub2_publish_cmd()
 
     async def pubsub_publish_cmd(self):
-        """
-        Posts a message to the given channel.
-          Return value:
-          - the number of clients that received the message.
-
-        :return: None
-        """
-        msg, channel = "Hello World!", 'TEST'
-        with await self.rd2 as conn2:
-            res1 = await conn2.publish(channel, msg)
-        frm = "PUBSUB_CMD - 'PUBLISH':PUB_RES - {0}\n"
-        logger.debug(frm.format(res1))
-
-    async def pubsub2_publish_cmd(self):
         """
         Posts a message to the given channel.
           Return value:
@@ -93,11 +78,9 @@ def main():
     rd1_conn = loop.run_until_complete(rd_client_factory(loop=loop, conf=conf['redis1']))
     rd2_conn = loop.run_until_complete(rd_client_factory(loop=loop, conf=conf['redis2']))
     rgc = RedisPubSubCommands(rd1_conn.rd, rd2_conn.rd, conf=conf['redis2'])
-    tsk1 = asyncio.ensure_future(RedisSubWorker.connect(rd1_conn.rd, 'TEST', conf=conf['redis1']))
+    asyncio.ensure_future(RedisSubWorker.connect(rd1_conn.rd, 'TEST', conf=conf['redis1']), loop=loop)
     try:
         loop.run_until_complete(rgc.run_pubsub_cmd())
-        loop.run_until_complete(asyncio.gather(tsk1))
-        # loop.run_in_executor(None, asyncio.gather(tsk1))
     except KeyboardInterrupt as e:
         logger.error("Caught keyboard interrupt {0}\nCanceling tasks...".format(e))
     finally:
